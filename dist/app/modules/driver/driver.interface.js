be916d038dcc084e9b4b6922c639034007a1d133
Your Name <khalidsaifullaha3@gmail.com>
2025-10-27 18:11:37 +0600

    some make change

--- dist/app/modules/driver/driver.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.carTypes = void 0;
-var carTypes;
-(function (carTypes) {
-    carTypes["car"] = "car";
-    carTypes["bike"] = "bike";
-    carTypes["cng"] = "cng";
-})(carTypes || (exports.carTypes = carTypes = {}));
--- dist/app/modules/user/user.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLE = void 0;
-var ROLE;
-(function (ROLE) {
-    ROLE["admin"] = "admin";
-    ROLE["rider"] = "rider";
-    ROLE["driver"] = "driver";
-})(ROLE || (exports.ROLE = ROLE = {}));

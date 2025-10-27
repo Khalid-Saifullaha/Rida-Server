5f4f3c7a1edc64722cce4a24b9aa0c7d337ce3f7
Your Name <khalidsaifullaha3@gmail.com>
2025-10-27 20:40:48 +0600

    Fix CORS and base URL for Vercel deploy

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://rida-server-qaj19plou-khalid-saifullahas-projects.vercel.app/",
+      "https://rida-server-qaj19plou-khalid-saifullahas-projects.vercel.app",
     ],
     credentials: true,
   })
